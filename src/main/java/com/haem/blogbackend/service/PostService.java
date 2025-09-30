35c56f6b01224aa02b598159d46c97789d4f70aa
haemin <goalsgoals0417@gmail.com>
2025-09-30 17:45:58 +0900

    feat: 게시글 작성 API 기본 구조 추가

--- src/main/java/com/haem/blogbackend/service/PostService.java
+++ src/main/java/com/haem/blogbackend/service/PostService.java
@@ -1,17 +1,26 @@
 package com.haem.blogbackend.service;
 
+import com.haem.blogbackend.domain.Admin;
+import com.haem.blogbackend.domain.Category;
+import com.haem.blogbackend.domain.Post;
+import com.haem.blogbackend.dto.request.PostCreateRequestDto;
+import com.haem.blogbackend.dto.response.PostResponseDto;
 import com.haem.blogbackend.dto.response.PostSummaryResponseDto;
+import com.haem.blogbackend.repository.AdminRepository;
 import com.haem.blogbackend.repository.PostRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PostService {
     private final PostRepository postRepository;
+    private final AdminRepository adminRepository;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, AdminRepository adminRepository) {
         this.postRepository = postRepository;
+        this.adminRepository = adminRepository;
     }
 
     public long getPostCount(){
@@ -20,5 +29,14 @@ public class PostService {
 
     public Page<PostSummaryResponseDto> getPosts(Pageable pageable){
         return postRepository.findAll(pageable).map(PostSummaryResponseDto::new);
+    }
+
+    @Transactional
+    public PostResponseDto createPost (PostCreateRequestDto requestDto, String accountName){
+        Admin admin = adminRepository.findByAccountName(accountName).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 계정입니다."));
+//        Category category =
+        Post post = new Post(admin, requestDto.getTitle(), requestDto.getContent());
+        Post saved = postRepository.save(post);
+        return new PostResponseDto(saved);
     }
 }
