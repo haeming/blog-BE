e65647fbf5d1ae5773b812bcc1094def621ee449
haeming <goalsgoals0417@gmail.com>
2025-09-30 23:33:41 +0900

    Update src/main/java/com/haem/blogbackend/controller/CategoryController.java
    Co-authored-by: Sangmin <sm9171@nate.com>

--- src/main/java/com/haem/blogbackend/controller/CategoryController.java
+++ src/main/java/com/haem/blogbackend/controller/CategoryController.java
@@ -14,7 +14,9 @@ import java.util.Map;
 public class CategoryController {
     private final CategoryService categoryService;
 
-    public CategoryController(CategoryService categoryService) { this.categoryService = categoryService; }
+public CategoryController(CategoryService categoryService) { 
+    this.categoryService = categoryService; 
+}
 
     @GetMapping("/allCount")
     public Map<String, Object> allCount(){
