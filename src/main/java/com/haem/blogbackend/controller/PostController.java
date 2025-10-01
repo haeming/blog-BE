ccd12cdcc035bbe1ccce90d4febbff72175e201a
haemin <goalsgoals0417@gmail.com>
2025-10-01 11:15:34 +0900

    refactor(category): 카테고리 개수 조회 메서드 네이밍 및 응답 타입 개선

--- src/main/java/com/haem/blogbackend/controller/PostController.java
+++ src/main/java/com/haem/blogbackend/controller/PostController.java
@@ -1,22 +1,25 @@
 package com.haem.blogbackend.controller;
 
-import com.haem.blogbackend.dto.response.PostSummaryResponseDto;
-import com.haem.blogbackend.service.PostService;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.haem.blogbackend.dto.response.PostSummaryResponseDto;
+import com.haem.blogbackend.service.PostService;
 
 @RestController
 @RequestMapping("/api/admin/post")
 public class PostController {
     private final PostService postService;
 
-    public PostController(PostService postService) { this.postService = postService; }
+    public PostController(PostService postService) { 
+        this.postService = postService;
+    }
 
     @GetMapping("/allCount")
     public Map<String, Object> allCount(){
