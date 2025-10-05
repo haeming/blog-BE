62da073e79d1b5395718a014ee7b98398d0f60b7
haemin <goalsgoals0417@gmail.com>
2025-10-05 14:18:11 +0900

    refactor: 카테고리 수정 로직 SRP 원칙 반영 및 엔드포인트 RESTful하게 개선

--- src/main/java/com/haem/blogbackend/controller/CategoryController.java
+++ src/main/java/com/haem/blogbackend/controller/CategoryController.java
@@ -1,26 +1,20 @@
 package com.haem.blogbackend.controller;
 
 import com.haem.blogbackend.domain.Category;
+import com.haem.blogbackend.dto.request.*;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.haem.blogbackend.dto.request.CategoryCreateRequestDto;
 import com.haem.blogbackend.dto.response.CategoryResponseDto;
 import com.haem.blogbackend.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import com.haem.blogbackend.dto.request.CategoryCreateRequestDto;
-import com.haem.blogbackend.dto.request.CategoryUpdateRequestDto;
-import com.haem.blogbackend.dto.response.CategoryResponseDto;
-import com.haem.blogbackend.service.CategoryService;
-
-import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -48,9 +42,14 @@ public CategoryController(CategoryService categoryService) {
         categoryService.deleteCategory(id);
     }
 
-    @PutMapping("/{id}")
-    public CategoryResponseDto updateCategory(@PathVariable("id") Long id, @Valid @RequestBody CategoryUpdateRequestDto requestDto){
-        return categoryService.updateCategory(id, requestDto);
+    @PatchMapping("/{id}/name")
+    public CategoryResponseDto updateCategoryName(@PathVariable("id") Long id, @Valid @RequestBody CategoryUpdateNameRequestDto requestDto){
+        return categoryService.updateCategoryName(id, requestDto);
+    }
+
+    @PatchMapping("/{id}/image")
+    public CategoryResponseDto updateCategoryImage(@PathVariable("id") Long id, @Valid @RequestBody CategoryUpdateImageRequestDto requestDto){
+        return categoryService.updateCategoryImage(id, requestDto);
     }
 
     @GetMapping
