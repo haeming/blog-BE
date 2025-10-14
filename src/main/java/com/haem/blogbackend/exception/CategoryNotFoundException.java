c975e225ab8ab8de75f8ecd4ff13400c7b29c9ef
haemin <goalsgoals0417@gamil.com>
2025-10-14 15:14:29 +0900

    feat: CategoryNotFoundException 상속 구조 변경

--- src/main/java/com/haem/blogbackend/exception/CategoryNotFoundException.java
+++ src/main/java/com/haem/blogbackend/exception/CategoryNotFoundException.java
@@ -1,6 +1,6 @@
 package com.haem.blogbackend.exception;
 
-public class CategoryNotFoundException extends RuntimeException{
+public class CategoryNotFoundException extends NotFoundException{
     public CategoryNotFoundException(Long id) {
         super("존재하지 않는 카테고리입니다. id=" + id);
     }
