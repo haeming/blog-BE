c36fb3460d17f77b3ff97fd91233f5eb1f74ed78
haemin <goalsgoals0417@gamil.com>
2025-10-22 15:07:38 +0900

    feat: PostController에 @Valid추가

--- src/main/java/com/haem/blogbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/haem/blogbackend/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.haem.blogbackend.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -52,6 +54,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(ApiResponse.error(ex.getMessage(), HttpStatus.UNAUTHORIZED.value()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<String>> handleValidationException(MethodArgumentNotValidException ex){
+        BindingResult bindingResult = ex.getBindingResult();
+        String message = bindingResult.getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(message, HttpStatus.BAD_REQUEST.value()));
     }
 
 }
