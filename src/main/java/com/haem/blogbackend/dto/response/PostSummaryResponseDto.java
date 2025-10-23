b8a8853b9cac98bf29f8ac0fa846c3ed06bdd1d8
haemin <goalsgoals0417@gmail.com>
2025-10-23 14:09:39 +0900

    feat: PostList 불러올 때 카테고리 Null 반환에 따른 Optional 처리

--- src/main/java/com/haem/blogbackend/dto/response/PostSummaryResponseDto.java
+++ src/main/java/com/haem/blogbackend/dto/response/PostSummaryResponseDto.java
@@ -11,28 +11,25 @@ public class PostSummaryResponseDto {
     private Long id;
     private String title;
     private String content;
-    private Long categoryId;
     private String categoryName;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    private PostSummaryResponseDto(Long id, String title, String content, Long categoryId, String categoryName, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    private PostSummaryResponseDto(Long id, String title, String content, String categoryName, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.categoryId = categoryId;
         this.categoryName = categoryName;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
 
-    public static PostSummaryResponseDto from(Post post){
+    public static PostSummaryResponseDto from(Post post, String categoryName){
         return new PostSummaryResponseDto(
                 post.getId(),
                 post.getTitle(),
                 post.getContent(),
-                post.getCategory().getId(),
-                post.getCategory().getCategoryName(),
+                categoryName,
                 post.getCreatedAt(),
                 post.getUpdatedAt()
         );
