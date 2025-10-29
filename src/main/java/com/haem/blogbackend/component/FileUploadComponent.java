b58f4a544031cde6bb56c83d270bb31e5c55df1d
haeming <goalsgoals0417@gmail.com>
2025-10-29 16:05:41 +0900

    refactor: 예외 처리 및 import 정리 (#24)
    * feat: 중복 예외처리 제거
    
    * chore: 불필요한 import 제거 및 경고(null-safety) 수정

--- src/main/java/com/haem/blogbackend/component/FileUploadComponent.java
+++ src/main/java/com/haem/blogbackend/component/FileUploadComponent.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Slf4j
