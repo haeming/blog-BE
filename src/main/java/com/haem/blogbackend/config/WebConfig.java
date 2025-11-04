0ea4d65d98b29a958fb72829443d059b5aa346f2
haemin <goalsgoals0417@gmail.com>
2025-11-04 21:39:23 +0900

    feat: 파일 업로드 경로 절대 경로 사용 및 도커 환경 설정 개선

--- src/main/java/com/haem/blogbackend/config/WebConfig.java
+++ src/main/java/com/haem/blogbackend/config/WebConfig.java
@@ -1,12 +1,17 @@
 package com.haem.blogbackend.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer{
 
     @Value("${file.upload-dir}")
@@ -14,8 +19,9 @@ public class WebConfig implements WebMvcConfigurer{
 
     @Override
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+        Path absolutePath = Paths.get(uploadDir).toAbsolutePath();
         registry.addResourceHandler("/uploadFiles/**")
-                .addResourceLocations("file:" + uploadDir);
+                .addResourceLocations("file:" + absolutePath + "/");
     }
 
 }
