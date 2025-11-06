ce60293d7a24a8fcf520770db6f4eb79a886ebd2
haemin <goalsgoals0417@gamil.com>
2025-11-06 16:51:49 +0900

    feat: 댓글 생성에 관한 requestDto/responseDto 생성

---
+++ src/main/java/com/haem/blogbackend/dto/response/CommentResponseDto.java
@@ -0,0 +1,46 @@
+package com.haem.blogbackend.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.haem.blogbackend.domain.Comment;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class CommentResponseDto {
+    private final Long id;
+    private final Long postId;
+    private final Long adminId;
+    private final Long parentId;
+    private final String nickname;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    private CommentResponseDto(Long id, Long postId, Long adminId, Long parentId, String nickname,
+                               String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.postId = postId;
+        this.adminId = adminId;
+        this.parentId = parentId;
+        this.nickname = nickname;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static CommentResponseDto from(Comment comment) {
+        return CommentResponseDto.builder()
+                .id(comment.getId())
+                .postId(comment.getPost().getId())
+                .adminId(comment.getAdmin() != null ? comment.getAdmin().getId() : null)
+                .parentId(comment.getParent() != null ? comment.getParent().getId() : null)
+                .nickname(comment.getNickname())
+                .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
+                .updatedAt(comment.getUpdatedAt())
+                .build();
+    }
+}
