f809049076781d67fae65a5cd9888d81dbbc2a40
haemin <goalsgoals0417@gmail.com>
2025-11-15 19:40:38 +0900

    refactor: warning 코드 정리 및 불필요한 import문 제거

--- src/main/java/com/haem/blogbackend/admin/controller/AuthController.java
+++ src/main/java/com/haem/blogbackend/admin/controller/AuthController.java
@@ -1,27 +1,20 @@
 package com.haem.blogbackend.admin.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.haem.blogbackend.admin.component.JwtProvider;
+import com.haem.blogbackend.admin.service.AdminService;
+import com.haem.blogbackend.common.exception.token.InvalidTokenException;
 import com.haem.blogbackend.domain.Admin;
 import com.haem.blogbackend.dto.request.AdminLoginRequestDto;
 import com.haem.blogbackend.dto.response.AdminLoginResponseDto;
 import com.haem.blogbackend.dto.response.TokenVerifyResponseDto;
-import com.haem.blogbackend.common.exception.token.ExpiredTokenException;
-import com.haem.blogbackend.common.exception.token.InvalidTokenException;
-import com.haem.blogbackend.admin.service.AdminService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AuthController {
     private final AdminService adminService;
 
-    public AuthController(AdminService adminService, JwtProvider jwtProvider) {
+    public AuthController(AdminService adminService) {
         this.adminService = adminService;
     }
 
@@ -34,9 +27,8 @@ public class AuthController {
     public TokenVerifyResponseDto verifyToken(@RequestHeader(value = "Authorization", required = false) String authHeader){
         String token = extractToken(authHeader);
         Admin admin = adminService.verifyAndGetAdmin(token);
-        TokenVerifyResponseDto response = TokenVerifyResponseDto.from(admin);
 
-        return response;
+        return TokenVerifyResponseDto.from(admin);
     }
 
     private String extractToken(String authHeader){
