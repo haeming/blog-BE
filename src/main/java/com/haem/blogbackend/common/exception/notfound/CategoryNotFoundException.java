ac681acb6e46438321aeb0f6bbea33001ed1162c
haemin <goalsgoals0417@gmail.com>
2025-11-24 21:35:57 +0900

    refactor: 모든 NotFoundException 메시지를 String.format 기반으로 통일

--- src/main/java/com/haem/blogbackend/common/exception/notfound/CategoryNotFoundException.java
+++ src/main/java/com/haem/blogbackend/common/exception/notfound/CategoryNotFoundException.java
@@ -4,6 +4,6 @@ import com.haem.blogbackend.common.exception.base.NotFoundException;
 
 public class CategoryNotFoundException extends NotFoundException {
     public CategoryNotFoundException(Long id) {
-        super("존재하지 않는 카테고리입니다. id=" + id);
+        super(String.format("존재하지 않는 카테고리입니다. id = %d", id));
     }
 }
