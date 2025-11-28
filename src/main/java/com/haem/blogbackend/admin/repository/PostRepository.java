50a79ed154664a1d08e169ecd81df7dbcfaf559c
haemin <goalsgoals0417@gmail.com>
2025-11-28 21:35:37 +0900

    feat: Post 수정 시 updatedAt 자동 갱신 기능 추가

--- src/main/java/com/haem/blogbackend/admin/repository/PostRepository.java
+++ src/main/java/com/haem/blogbackend/admin/repository/PostRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findByTitleContaining(String title, Pageable pageable);
-    Page<Post> findPostByCategoryContaining(String categoryName, Pageable pageable);
     Page<Post> findByDeletedAtIsNull(Pageable pageable);
 
     long countByCategoryId(Long categoryId);
