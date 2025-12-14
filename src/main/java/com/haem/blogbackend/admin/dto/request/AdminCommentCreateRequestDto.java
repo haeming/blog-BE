4d15cdbc7d627ce88dccaa13bce071a18bd9abda
haemin <goalsgoals0417@gmail.com>
2025-12-14 11:15:29 +0900

    refactor: admin 댓글 생성 DTO에서 불필요한 필드 제거

--- src/main/java/com/haem/blogbackend/admin/dto/request/AdminCommentCreateRequestDto.java
+++ src/main/java/com/haem/blogbackend/admin/dto/request/AdminCommentCreateRequestDto.java
@@ -7,32 +7,26 @@ import lombok.Getter;
 
 @Builder
 @Getter
-public class CommentCreateRequestDto {
+public class AdminCommentCreateRequestDto {
     @NotNull
     private Long postId;
-    private Long adminId;
     private Long parentId;
-    private String nickname;
 
     @NotBlank(message = "댓글 내용은 필수입니다.")
     private String content;
 
-    protected CommentCreateRequestDto() {}
+    protected AdminCommentCreateRequestDto() {}
 
-    private CommentCreateRequestDto(Long postId, Long adminId, Long parentId, String nickname, String content) {
+    private AdminCommentCreateRequestDto(Long postId, Long parentId, String content) {
         this.postId = postId;
-        this.adminId = adminId;
         this.parentId = parentId;
-        this.nickname = nickname;
         this.content = content;
     }
 
-    public static CommentCreateRequestDto from(Long postId, Long adminId, Long parentId, String nickname, String content){
-        return CommentCreateRequestDto.builder()
+    public static AdminCommentCreateRequestDto from(Long postId, Long parentId, String content){
+        return AdminCommentCreateRequestDto.builder()
                 .postId(postId)
-                .adminId(adminId)
                 .parentId(parentId)
-                .nickname(nickname)
                 .content(content)
                 .build();
     }
