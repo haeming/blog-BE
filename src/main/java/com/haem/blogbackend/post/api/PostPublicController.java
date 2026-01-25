6ef936d3c3622da3ed79a148684ab4e6799a2910
haemin <goalsgoals0417@gmail.com>
2026-01-25 21:45:20 +0900

    feat: 비로그인 사용자 게시글 조회용 Public 컨트롤러 추가

---
+++ src/main/java/com/haem/blogbackend/post/api/PostPublicController.java
@@ -0,0 +1,9 @@
+package com.haem.blogbackend.post.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostPublicController {
+}
